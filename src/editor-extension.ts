0de2bc5e5712ab6ecb55535318df94433916f847
Shane Lamb <shanelamb1@gmail.com>
2025-02-15 13:26:00 +1000

    improve logic in CM plugin for detecting LLM file

--- src/editor-extension.ts
+++ src/editor-extension.ts
@@ -13,42 +13,40 @@ import { RangeSetBuilder, Text } from '@codemirror/state'
 import { Editor, editorInfoField, getIcon, TFile } from 'obsidian'
 import { fileEvents, getLlmDocsPlugin, isFileBeingProcessed } from './registry'
 
+type PosRange = { from: number, to: number }
+
 class LlmDocsCodemirrorPlugin implements PluginValue {
 	decorations: DecorationSet
-	file: TFile | null = null
-	editor?: Editor
 
-	constructor(view: EditorView) {
-		this.decorations = this.buildDecorations(view)
+	constructor(private readonly view: EditorView) {
+		this.decorations = this.buildDecorations()
 	}
 
 	update(update: ViewUpdate) {
 		if (update.docChanged || update.viewportChanged) {
-			// don't apply styling if no "model" property on frontmatter
-			const info = update.view.state.field(editorInfoField)
-			this.file = info.file
-			this.editor = info.editor
-			if (!this.file || !this.editor) {
-				this.decorations = Decoration.none
-				return
-			}
-			const frontmatter = info.app.metadataCache.getFileCache(this.file)?.frontmatter
-			if (!frontmatter || !frontmatter.model) {
-				this.decorations = Decoration.none
-				return
-			}
-
-			this.decorations = this.buildDecorations(update.view)
+			this.decorations = this.buildDecorations()
 		}
 	}
 
-	buildDecorations(view: EditorView): DecorationSet {
+	buildDecorations(): DecorationSet {
+		if (!this.enabled()) {
+			return Decoration.none
+		}
+
+		const {state, viewport} = this.view
+		const {doc} = state
+
+		const {file, editor} = state.field(editorInfoField)
+		if (!file || !editor) {
+			// todo: unsure if this code would ever be reached
+			console.log('Could not decorate: missing file or editor', {file, editor})
+			return Decoration.none
+		}
+
 		const builder = new RangeSetBuilder<Decoration>()
 
-		const { from, to } = view.viewport
-		const { doc } = view.state
-		const startLine = doc.lineAt(from)
-		const endLine = doc.lineAt(to)
+		const startLine = doc.lineAt(viewport.from)
+		const endLine = doc.lineAt(viewport.to)
 		let offset = startLine.from
 		let promptStart: number | null = null
 		for (const line of doc.iterLines(startLine.number, endLine.number)) {
@@ -85,12 +83,19 @@ class LlmDocsCodemirrorPlugin implements PluginValue {
 			offset += line.length + 1
 		}
 
-		this.addCompleteButton(to, promptStart, doc, builder)
+		this.addCompleteButton(viewport, promptStart, doc, builder, file, editor)
 
 		return builder.finish()
 	}
 
-	addCompleteButton(viewportEnd: number, promptStart: number | null, doc: Text, builder: RangeSetBuilder<Decoration>) {
+	addCompleteButton(
+		viewport: PosRange,
+		promptStart: number | null,
+		doc: Text,
+		builder: RangeSetBuilder<Decoration>,
+		file: TFile,
+		editor: Editor
+	) {
 		if (!promptStart) {
 			return
 		}
@@ -99,7 +104,7 @@ class LlmDocsCodemirrorPlugin implements PluginValue {
 			return
 		}
 		const lastLine = doc.line(doc.lines)
-		if (doc.length > viewportEnd) {
+		if (doc.length > viewport.to) {
 			return
 		}
 		const pos = lastLine.to
@@ -107,24 +112,32 @@ class LlmDocsCodemirrorPlugin implements PluginValue {
 			pos,
 			pos,
 			Decoration.widget({
-				widget: new CompleteWidget(this.editor!, this.file!),
+				widget: new CompleteWidget(editor, file),
 				side: 1
 			})
 		)
 	}
 
+	enabled() {
+		// don't apply decorations if no "model" property on frontmatter
+		const lines = this.view.state.doc.iterLines()
+		if (lines.next().value !== '---') {
+			return false // no frontmatter
+		}
+		for (const line of lines) {
+			if (line.startsWith('model:')) {
+				return true
+			}
+			if (line === '---') {
+				return false
+			}
+		}
+		return false
+	}
+
 	destroy() {
 	}
 }
-
-const pluginSpec: PluginSpec<LlmDocsCodemirrorPlugin> = {
-	decorations: (value: LlmDocsCodemirrorPlugin) => value.decorations,
-}
-
-export const llmDocsCodemirrorPlugin = ViewPlugin.fromClass(
-	LlmDocsCodemirrorPlugin,
-	pluginSpec
-)
 
 export class CompleteWidget extends WidgetType {
 	private button: HTMLButtonElement
@@ -145,10 +158,8 @@ export class CompleteWidget extends WidgetType {
 		const button = this.button
 		button.innerText = 'Complete'
 		button.onclick = async () => {
-			if (this.editor && this.file) {
-				this.editor.focus()
-				await getLlmDocsPlugin().completeDoc(this.editor, this.file)
-			}
+			this.editor.focus()
+			await getLlmDocsPlugin().completeDoc(this.editor, this.file)
 		}
 		button.style.position = 'absolute'
 		button.style.top = '100%'
@@ -179,7 +190,16 @@ export class CompleteWidget extends WidgetType {
 	}
 
 	destroy(dom: HTMLElement) {
-		super.destroy(dom)
 		fileEvents.off('change', this.onEvent)
+		super.destroy(dom)
 	}
 }
+
+const pluginSpec: PluginSpec<LlmDocsCodemirrorPlugin> = {
+	decorations: (value: LlmDocsCodemirrorPlugin) => value.decorations,
+}
+
+export const llmDocsCodemirrorPlugin = ViewPlugin.fromClass(
+	LlmDocsCodemirrorPlugin,
+	pluginSpec
+)
